b8494c0b4e1cf4a53ba041e5f5081077dce02b18
CW - Roberto Olvera Hernandez <rolvhdez@ccg.unam.mx>
2025-10-13 13:20:53 +0000

    [changed] code to PEP8 standard

--- utils/01_map_genes.R
+++ utils/01_map_genes.R
@@ -20,25 +20,21 @@ ncbi_gene_mappings$GENE_ID <- as.character(ncbi_gene_mappings$GENE_ID)
 
 # Combine all results
 full_gene_map <- fgwas_results %>% 
-  inner_join(ensemble_ids, ., by="SNP") %>% 
-  inner_join(ncbi_gene_mappings, ., by="GENE_ID") %>% 
+  inner_join(ensemble_ids, ., by = "SNP") %>% 
+  inner_join(ncbi_gene_mappings, ., by = "GENE_ID") %>% 
   arrange(CHR, BP)
-if (!length(ensemble_ids) == 0 & length(ncbi_gene_mappings) == 0) {
-  cli_alert_success("Exported `" %&% output_dir %&% "gene_mappings.csv`")
-  write.csv(
+
+# Export to text file (gzipped)
+if (nrow(full_gene_map) > 0) {
+  gz <- gzfile("gene_mappings.sumstats.gz", "w")
+  write.table(
     full_gene_map,
-    file = output_dir %&% "gene_mappings.csv",
+    file = output_dir %&% gz,
+    sep = " ",
     row.names = FALSE
   )
+  close(gz)
+  cli_alert_success("Exported `" %&% output_dir %&% "gene_mappings.sumstats.gz`")
+} else {
+  cli_alert_warning('Queries returned no results. No `.csv` will be produced.')
 }
-cli_alert_warning('Queries returned no results. No `.csv` will be produced.')
-
-top5_mapping <- full_gene_map %>% 
-  slice_min(order_by = P, n = 5) %>% 
-  arrange(P)
-cli_alert_success("Exported `" %&% output_dir %&% "top5_mappings.csv`")
-write.csv(
-  top5_mapping,
-  file = output_dir %&% "top5_mappings.csv",
-  row.names = FALSE
-)
