2614533531271a3a823800cb2b470bb3f359a77b
CW - Roberto Olvera Hernandez <rolvhdez@ccg.unam.mx>
2025-10-22 13:16:59 +0000

    [changed] output messages; histogram for whole genome; plots for 10 random sibpairs

--- ibd-plots.R
+++ ibd-plots.R
@@ -1,37 +1,30 @@
 # Author: Roberto Olvera Hernandez
 # Date: 2025-10-21
+suppressPackageStartupMessages(library(cli))
 suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(ggplot2))
 
-args <- commandArgs(trailingOnly = TRUE)
-ibd_path <- args[1]
-output_dir <- args[2]
-
-ibd <- data.table::fread(ibd_path, header = TRUE, sep = "\t")
-
 # DECLARING FUNCTIONS
 "%&%" <- function(a, b) paste0(a, b)
+source("utils/00_utils.R")
 
 check_out <- function(x) if (!dir.exists(x)) dir.create(x)
-
 siblings_ibd_plots <- function(x) {
   require(ggplot2)
   # Take two random siblings from the IBD file
   sib_sample <- x[x$id1 == sample(x$id1, 1), ]
   sib_sample <- sib_sample[sib_sample$id2 == sample(sib_sample$id2, 1)]
-
   # Get the IID's of the siblings
-  sib1 <- unique(x$id1)
-  sib2 <- unique(x$id2)
-
+  sib1 <- unique(sib_sample$id1)
+  sib2 <- unique(sib_sample$id2)
   # Make the plot
   ibd_colors <- c("IBD0" = "white", "IBD1" = "dodgerblue2", "IBD2" = "firebrick2")
   g <- ggplot(data = sib_sample) +
       geom_rect(aes(xmin = start, xmax = stop, ymin = 0, max = 0.9, fill = ibd), color = "black", linewidth = 0.80) +
       facet_grid(chr ~ .) +
       xlab("Position (Mbp)") +
-      labs(title = paste0(sib1, "_", sib2), fill = "IBD Type") +
+      labs(title = paste0(sib1, ":", sib2), fill = "IBD Type") +
       scale_fill_manual(values = ibd_colors, drop = FALSE) +
       theme_minimal() +
       theme(
@@ -43,8 +36,7 @@ siblings_ibd_plots <- function(x) {
       )
   return(g)
 }
-
-density_length_plot <- function(x) {
+chr_density_length_plot <- function(x) {
   g <- ggplot(data = x) +
     geom_density(aes(x = length, color = chr), linewidth = 1) +
     facet_wrap(. ~ chr) +
@@ -53,11 +45,34 @@ density_length_plot <- function(x) {
     theme_minimal()
   return(g)
 }
-
+histogram_length_plot <- function(x) {
+  median_length = median(x$length)
+  g <- ggplot(data = x) +
+    geom_histogram(
+      aes(x = length),
+      fill = "steelblue", color = "black",
+      boundary = 0, binwidth = 50 / 10
+    ) +
+    geom_vline(
+      xintercept = median_length,
+      color = "red", linetype = "dashed"
+    ) +
+    xlab("Segment length (cM)") +
+    ylab("No. segments") +
+    labs(
+      caption = paste0("Median segment length: ", round(median_length, 2), "cM")
+    ) +
+    scale_y_continuous(
+      labels = scales::scientific
+    ) +
+    scale_x_continuous(
+      breaks = seq(0, 250, 25)
+    ) +
+    theme_minimal()
+}
 create_plots <- function(data) {
   require(dplyr)
   require(ggplot2)
-
   plots <- list()
   data <- data %>%
     select( # simpler column names
@@ -75,29 +90,62 @@ create_plots <- function(data) {
       start = start / 1e6,
       stop = stop / 1e6
     )
-
   # Create the plots
-  plots[[1]] <- density_length_plot(data)
-  for (i in 2:6) plots[[i]] <- siblings_ibd_plots(data)
+  plots[[1]] <- chr_density_length_plot(data)
+  names(plots)[1] <- "shared_length_chr_density.png"
 
-  # Name the plots
-  names(plots)[1] <- "chr_density.png"
-  for (i in 2:6) {
-    j <- i - 1
-    names(plots)[i] <- paste0("ibd-map_FS", sprintf("%02d", j), ".png")
+  plots[[2]] <- histogram_length_plot(data)
+  names(plots)[2] <- "shared_length_histogram.png"
+
+  # Generate plots for 10 random sibling pairs
+  num_fs_plots <- 10
+  start_index <- length(plots) + 1
+  end_index <- start_index + num_fs_plots - 1
+  for(i in start_index:end_index) {
+      j <- i - start_index + 1
+      fs_num <- sprintf("%02d", j)
+      plots[[i]] <- siblings_ibd_plots(data)
+      names(plots)[i] <- paste0("ibd-mosaic_FS", fs_num, ".png")
   }
   return(plots)
 }
-
 export_plot <- function(plot, file_name) {
-  png(file_name, height = 1080, width = 1920, res = 150, units = "px")
-  print(plot)
-  dev.off()
+  tryCatch(
+    {
+      #' Try exporting the 
+      png(file_name, height = 1080, width = 1920, res = 150, units = "px")
+      print(plot)
+      suppressMessages(invisible(dev.off()))
+    },
+    error = function(cond) {
+      cli::cli_alert_danger("Something happened during exportation.")
+      message(conditionMessage(cond))
+      NA
+    },
+    finally = {
+      cli::cli_alert_success("Exported " %&% file_name)
+    }
+  )
 }
 
 # --- MAIN EXECUTION ---
+args <- commandArgs(trailingOnly = TRUE)
+ibd_path <- args[1]
+output_dir <- args[2]
 check_out(output_dir)
-
-# Same result as Map but with slightly different syntax
-plots <- create_plots(ibd)
-mapply(export_plot, plots, output_dir %&% names(plots))
+ibd <- fancy_process(
+  process = data.table::fread,
+  message = "Reading " %&% ibd_path,
+  # Function parameters
+  file = ibd_path,
+  header = TRUE,
+  sep = "\t"
+)
+list_plots <- fancy_process(
+  process = create_plots,
+  message = "Creating plots",
+  # Function parameters
+  data = ibd
+)
+# Export the plots
+mapply(export_plot, list_plots, output_dir %&% names(list_plots))
