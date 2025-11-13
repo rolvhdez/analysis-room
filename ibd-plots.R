088ab5620cbe883c1873c7b051a98bf1a3595519
WSL - Roberto Olvera Hernandez <rolvhdez@ccg.unam.mx>
2025-11-13 13:16:29 +0000

    [added] chromosome sectioning for the histogram plots

--- ibd-plots.R
+++ ibd-plots.R
@@ -45,7 +45,8 @@ chr_density_length_plot <- function(x) {
     theme_minimal()
   return(g)
 }
-histogram_length_plot <- function(x) {
+histogram_length_plot <- function(x, chrom = 1:22) {
+  x <- x[x$chr %in% chrom]
   median_length = median(x$length)
   g <- ggplot(data = x) +
     geom_histogram(
@@ -97,8 +98,18 @@ create_plots <- function(data) {
   plots[[2]] <- histogram_length_plot(data)
   names(plots)[2] <- "shared_length_histogram.png"
 
-  # Generate plots for 10 random sibling pairs
-  num_fs_plots <- 10
+  # Histogram plot but for each chromosome
+  start_index <- length(plots) + 1
+  end_index <- start_index + 22 - 1
+  for(i in start_index:end_index) {
+    j <- i - start_index + 1
+    chr_num <- sprintf("%02d", j)
+    plots[[i]] <- histogram_length_plot(data, chrom = j)
+    names(plots)[i] <- paste0("shared_length_chr", chr_num, "_histogram.png")
+  }
+
+  # Generate plots for 5 random sibling pairs
+  num_fs_plots <- 5
   start_index <- length(plots) + 1
   end_index <- start_index + num_fs_plots - 1
   for(i in start_index:end_index) {
@@ -141,6 +152,40 @@ ibd <- fancy_process(
   header = TRUE,
   sep = "\t"
 )
+
+ibd_types <- ibd %>%
+  mutate(IBDType = as.factor(IBDType)) %>%
+  group_by(Chr, IBDType) %>%
+  summarise(Count = n()) %>%
+  group_by(Chr) %>%
+  mutate(IBDProp = Count / sum(Count)) %>%
+  ungroup()
+
+print(ibd_types %>% filter(Chr == 22))
+
+segment_number <- ibd %>%
+  group_by(Chr) %>%
+  summarise(Count = n()) %>%
+  ggplot() +
+  geom_bar(aes(x = Chr, y = Count), stat = "identity",
+  color = "black", fill = "white") +
+  ylab("No. segments") +
+  scale_x_continuous(breaks = seq(1,22,1))
+export_plot(segment_number, output_dir %&% "number_segments_chr.png")
+
+ibd_colors <- c("IBD0" = "white", "IBD1" = "dodgerblue2", "IBD2" = "firebrick2")
+
+ibd_prop_plot <- ibd_types %>%
+  mutate(IBDType = as.factor(paste0("IBD", IBDType))) %>%
+  ggplot(aes(x = Chr, fill = IBDType, y = IBDProp)) +
+  geom_bar(stat = "identity", color = "black") +
+  scale_fill_manual(values = ibd_colors, drop = FALSE) +
+  labs(y = "Proportion within Chromosome", x = "Chromosome") +
+  scale_x_continuous(breaks = seq(1,22,1))
+
+export_plot(ibd_prop_plot, output_dir %&% "ibd_proportions_chr.png")
+q()
+
 list_plots <- fancy_process(
   process = create_plots,
   message = "Creating plots",
