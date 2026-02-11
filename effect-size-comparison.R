52428b70350a720e9bd25aa88b544a12457e738f
WSL - Roberto Olvera Hernandez <rolvhdez@ccg.unam.mx>
2026-02-10 18:02:42 -0600

    [changed] new plot and functionality

--- effect-size-comparison.R
+++ effect-size-comparison.R
@@ -1,5 +1,7 @@
 # Author:
 # Date: 
+renv::load("~/repositories/analysis-room/")
+setwd("~/repositories/analysis-room/")
 suppressPackageStartupMessages(library(cli))
 suppressPackageStartupMessages(library(ggrepel))
 suppressPackageStartupMessages(library(ggplot2))
@@ -16,25 +18,29 @@ source("utils/01_map_genes.R")
 
 # Parameter ---
 args <- commandArgs(trailingOnly = TRUE)
-snipar_sumstats <- args[1]
-regenie_sumstats <- args[2]
+sumstats_a <- args[1]
+sumstats_b <- args[2]
 output_dir <- args[3]
 phenotype <- if (length(args) < 4) "" else args[4] # Default: empty
+model_a <- if (length(args) < 5) "snipar" else args[5]
+model_b <- if (length(args) < 6) "snipar" else args[6]
+model_label_a <- if (length(args) < 7) "Model A" else args[7]
+model_label_b <- if (length(args) < 8) "Model B" else args[8]
 
 # Check parameters ----
-if (!file.exists(snipar_sumstats)) {
-  print(snipar_sumstats)
+if (!file.exists(sumstats_a)) {
+  print(sumstats_a)
   # Check if the summary statistics exist
   cli_abort(c(
-    "{snipar_sumstats} does not exist",
+    "{sumstats_a} does not exist",
     "x" = "You've supplied a file that does not exist."
   ))
 }
-if (!file.exists(regenie_sumstats)) {
-  print(regenie_sumstats)
+if (!file.exists(sumstats_b)) {
+  print(sumstats_b)
   # Check if the summary statistics exist
   cli_abort(c(
-    "{regenie_sumstats} does not exist",
+    "{sumstats_b} does not exist",
     "x" = "You've supplied a file that does not exist."
   ))
 }
@@ -44,103 +50,140 @@ if (!dir.exists(output_dir)) {
 }
 
 #------------------------------------
-# Read the summary statistics -------
-df_snipar <- fancy_process(
-  process = read_sumstats_file,
-  message = "Reading " %&% snipar_sumstats,
-  # Function parameters
-  sumstats_path = snipar_sumstats,
-  chunk_size = 1000000
-)
-df_snipar <- reformat_sumstats(df_snipar, "snipar")
-df_snipar$MODEL <- "snipar"
-#
-df_regenie <- fancy_process(
-  process = read_sumstats_file,
-  message = "Reading " %&% regenie_sumstats,
-  # Function parameters
-  sumstats_path = regenie_sumstats,
-  chunk_size = 1000000
-)
-df_regenie <- reformat_sumstats(df_regenie, "regenie")
-df_regenie$SNP <- paste0("chr", df_regenie$SNP)
-df_regenie$MODEL <- "regenie"
-df_regenie <- df_regenie %>%
-  dplyr::filter(SNP %in% unique(df_snipar$SNP))
-#
-df_sumstats <- dplyr::bind_rows(df_snipar, df_regenie)
 
-bonferroni <- 5e-8
-sig_k <- df_sumstats %>% filter(P <= bonferroni) %>% pull(SNP)
-if (length(sig_k) > 0) {
-  cli::cli_alert_warning(scales::comma(length(sig_k)) %&% " SNPs found at p <= " %&% bonferroni)
-  genes <- create_gene_ranges()
-  df_annotations <- annotate_genes_to_sig_snps(
-    sumstats = df_sumstats,
-    gene_range = genes,
-    sig = bonferroni
-  )
-  df_annotations <- df_annotations %>% select(SNP, GENE)
-} else {
-  cli::cli_alert_warning("No significant SNPs were found at p <= " %&% bonferroni %&% ". Skipping annotation.")
-}
+# Read the summary statistics -------
+df_a <- fancy_process(
+  process = read_sumstats_file,
+  message = "Reading " %&% sumstats_a,
+  # Function parameters
+  sumstats_path = sumstats_a,
+  chunk_size = 1000000
+)
+df_a <- reformat_sumstats(df_a, model_a)
+df_a$MODEL <- "model_a"
+
+#
+df_b <- fancy_process(
+  process = read_sumstats_file,
+  message = "Reading " %&% sumstats_b,
+  # Function parameters
+  sumstats_path = sumstats_b,
+  chunk_size = 1000000
+)
+df_b <- reformat_sumstats(df_b, model_b)
+df_b$MODEL <- "model_b"
+
+#
+df_sumstats <- dplyr::bind_rows(df_a, df_b)
+
+# #
+# bonferroni <- 5e-8
+# # Check that there are significant SNPs to annotate
+# sig_k <- df_sumstats %>% filter(P <= bonferroni) %>% pull(SNP)
+# if (length(sig_k) > 0) {
+#   cli::cli_alert_warning(scales::comma(length(sig_k)) %&% " SNPs found at p <= " %&% bonferroni)
+#   genes <- create_gene_ranges()
+#   df_annotations <- annotate_genes_to_sig_snps(
+#     sumstats = df_sumstats,
+#     gene_range = genes,
+#     sig = bonferroni
+#   )
+#   df_annotations <- df_annotations %>% select(SNP, GENE)
+#   df_sumstats <- df_sumstats %>%
+#     left_join(df_annotations, by = "SNP")
+# } else {
+#   cli::cli_alert_warning("No significant SNPs were found at p <= " %&% bonferroni %&% ". Skipping annotation.")
+# }
 
 # Correlations ---
-df_wide <- df_sumstats %>%
-  pivot_wider(
+effects <- df_sumstats %>%
+  tidyr::pivot_wider(
     id_cols = SNP,
     names_from = MODEL,
     values_from = BETA
-  )
-snp_regenie_significant <- df_sumstats %>%
-  filter(P < 5e-08 & MODEL == "regenie") %>%
-  pull(SNP)
-snp_snipar_significant <- df_sumstats %>%
-  filter(P < 5e-08 & MODEL == "snipar") %>%
-  pull(SNP)
-df_wide <- df_wide %>%
-  mutate(
-    regenie_significant = ifelse(SNP %in% snp_regenie_significant, TRUE, FALSE),
-    snipar_significant = ifelse(SNP %in% snp_snipar_significant, TRUE, FALSE)
-  )
-df_wide <- df_wide %>%
-  left_join(df_annotations, by = "SNP")
+  ) %>%
+  tidyr::drop_na()
+pvalues <- df_sumstats %>%
+  tidyr::pivot_wider(
+    id_cols = SNP,
+    names_from = MODEL,
+    values_from = P
+  ) %>%
+  tidyr::drop_na()
+effect_pval <- effects %>%
+  left_join(pvalues, by = "SNP") %>%
+  rename_with(~ gsub("\\.x$", "_effect", .)) %>%
+  rename_with(~ gsub("\\.y$", "_pval", .))
 
-corr_plot <- ggplot() +
-  geom_hline(yintercept = 0, color = "gray", linewidth = 0.5, linetype = "dashed") +
-  geom_vline(xintercept = 0, color = "gray", linewidth = 0.5, linetype = "dashed") +
-  geom_point(
-    data = df_wide,
-    aes(x = regenie, y = snipar),
-    color = "gray", alpha = 0.25
-  ) +
-  geom_point(
-    data = filter(df_wide, regenie_significant == TRUE),
-    aes(x = regenie, y = snipar, color = "regenie"),
-  ) +
-  geom_point(
-    data = filter(df_wide, snipar_significant == TRUE),
-    aes(x = regenie, y = snipar, color = "snipar"),
-  ) +
-  geom_text_repel(
-    data = filter(df_wide, !is.na(GENE) & snipar_significant == TRUE),
-    aes(x = regenie, y = snipar, label = GENE),
-    box.padding = 0.5,
-    point.padding = 0.3,
-    max.overlaps = 16,
-    size = 3
-  ) +
-  ylab(expression(delta[snipar])) +
-  xlab(expression(beta[regenie])) +
-  labs(
-    title = phenotype
-  ) +
-  scale_color_manual(
-    name = "Genome-wide significance (p < 5e-8):",
-    values = c(
-      "regenie" = blue,
-      "snipar" = red
+r_value <- cor(effects$model_a, effects$model_b, method = "pearson")
+r_value_msg <- paste0(
+  "Pearson's r^2 = ", round(r_value, 4)
+)
+cli::cli_alert_info(r_value_msg)
+
+#
+# Create conditional axis labels
+x_label <- if(tolower(model_a) == "snipar") {
+  bquote(delta[.(model_label_a)])
+} else {
+  bquote(beta[.(model_label_a)])
+}
+y_label <- if(tolower(model_b) == "snipar") {
+  bquote(delta[.(model_label_b)])
+} else {
+  bquote(beta[.(model_label_b)])
+}
+
+p <- effect_pval %>%
+  mutate(
+    significance = case_when(
+      model_a_pval < 5e-8 & model_b_pval >= 5e-8 ~ model_label_a,
+      model_b_pval < 5e-8 & model_a_pval >= 5e-8 ~ model_label_b,
+      model_a_pval < 5e-8 & model_b_pval < 5e-8 ~ "Both",
+      TRUE ~ "Neither"
     )
+  ) %>%
+  ggplot(aes(x = model_a_effect, y = model_b_effect)) +
+  # First layer: All "Neither" points with transparency
+  geom_point(
+    data = . %>% filter(significance == "Neither"),
+    aes(fill = significance),
+    shape = 21,
+    color = "gray",
+    size = 1,
+    alpha = 0.5
   ) +
-  theme(legend.position = "top")
-export_plot(corr_plot, output_dir %&% "correlation_plot.png")
+  # Second layer: Significant points (no transparency)
+  geom_point(
+    data = . %>% filter(significance != "Neither"),
+    aes(fill = significance),
+    shape = 21,
+    color = "black",
+    size = 2
+  ) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "red") +
+  annotate("text",
+    x = 0,
+    y = max(effect_pval$model_b_effect),
+    label = paste("r = ", round(r_value, 4)),
+    hjust = 0, vjust = 1, size = 3, color = "black"
+  ) +
+  scale_fill_manual(
+    values = c(
+      setNames(blue, model_label_a),
+      setNames(green, model_label_b),
+      "Both" = red,
+      "Neither" = "lightgray"
+    ),
+    name = "Is significant (p < 5e-8)"
+  ) +
+  labs(title = phenotype) +
+  xlab(x_label) +
+  ylab(y_label) +
+  theme(
+    legend.position = "top",
+    legend.title = element_text(size = 10)
+  )
+export_plot(p,
+  output_dir %&% "effect_scatter." %&% model_a %&% "-" %&% model_b %&% ".png"
+)
